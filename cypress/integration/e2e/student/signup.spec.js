0eeb364de3340106a36466dd719a370a2cb05138
Muhammad Ehtsham <ehtsham@rnssol.com>
2021-10-01 16:04:27 +0500

    Sign up assertion

--- cypress/integration/e2e/student/signup.spec.js
+++ cypress/integration/e2e/student/signup.spec.js
@@ -9,6 +9,10 @@ describe("Signup", () => {
 
   it("should register user", () => {
     cy.registerStudent(email, "Test", "User", "Test@12345", "Test@12345");
+    cy.get(".Toastify__toast-body").should(
+      "have.text",
+      "Email has been sent to you for verification, Kindly verify your email to get access to the system."
+    );
   });
 
   afterEach(() => {
